cbcb6ed9b4a8d9892637525e8c46a1c0cb539544
WildRikku <119439592+WildRikku@users.noreply.github.com>
2024-05-27 03:35:17 +0200

    optimization

--- Assets/Scripts/ExplodeOnImpact.cs
+++ Assets/Scripts/ExplodeOnImpact.cs
@@ -3,35 +3,31 @@ using UnityEngine;
 
 public class ExplodeOnImpact : MonoBehaviour
 {
-    [SerializeField]
-    public BasicPaintableLayer primaryLayer;
-    [SerializeField]
-    public BasicPaintableLayer secondaryLayer;
+    [SerializeField] public BasicPaintableLayer primaryLayer;
+    [SerializeField] public BasicPaintableLayer secondaryLayer;
 
-    private void OnCollisionEnter2D(Collision2D col) 
+    private PaintingParameters _paintingParameters = new PaintingParameters()
     {
-        int circleSize = 50;
-        Shape destroyCircle = Shape.GenerateShapeCircle(circleSize);
+        Color = Color.clear,
+        Position = new Vector2Int(),
+        Shape = Shape.GenerateShapeCircle(50),
+        PaintingMode = PaintingMode.REPLACE_COLOR,
+        DestructionMode = DestructionMode.DESTROY
+    };
 
+    private void OnCollisionEnter2D(Collision2D col)
+    {
         Vector2 position = col.GetContact(0).point;
-        primaryLayer?.Paint(new PaintingParameters() 
-        {
-            Color = Color.clear, 
-            Position = new Vector2Int((int)(position.x * primaryLayer.PPU) - circleSize, (int)(position.y * primaryLayer.PPU) - circleSize), 
-            Shape = destroyCircle, 
-            PaintingMode=PaintingMode.REPLACE_COLOR,
-            DestructionMode = DestructionMode.DESTROY
-        });
+        _paintingParameters.DestructionMode = DestructionMode.DESTROY;
+        _paintingParameters.Position.x = (int)(position.x * primaryLayer.PPU) - 50;
+        _paintingParameters.Position.y = (int)(position.y * primaryLayer.PPU) - 50;
+        primaryLayer.Paint(_paintingParameters
+        );
 
-        secondaryLayer?.Paint(new PaintingParameters() 
-        {
-            Color = Color.clear,
-            Position = new Vector2Int((int)(position.x * secondaryLayer.PPU) - circleSize, (int)(position.y * secondaryLayer.PPU) - circleSize), 
-            Shape = destroyCircle, 
-            PaintingMode=PaintingMode.REPLACE_COLOR,
-            DestructionMode = DestructionMode.NONE
-        });
-        
+        _paintingParameters.DestructionMode = DestructionMode.NONE;
+        secondaryLayer.Paint(_paintingParameters
+        );
+
         Destroy(gameObject);
     }
 }
