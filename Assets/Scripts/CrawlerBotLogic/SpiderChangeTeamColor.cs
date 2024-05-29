4bd95f3fe23962e5050ba94befa48917695d3c9a
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2024-05-29 05:23:51 +0200

    team color stuff

--- Assets/Scripts/CrawlerBotLogic/SpiderChangeTeamColor.cs
+++ Assets/Scripts/CrawlerBotLogic/SpiderChangeTeamColor.cs
@@ -5,4 +5,21 @@ using UnityEngine;
 public class SpiderChangeTeamColor : MonoBehaviour
 {
     public int teamColor;
+
+    public SpriteRenderer leftHip, rightHip;
+    public SpriteRenderer leftLeg, rightLeg;
+    public SpriteRenderer head;
+
+    public List<Sprite> hips, legs, heads;
+
+    private void Start()
+    {
+        leftHip.sprite = hips[teamColor];
+        rightHip.sprite = hips[teamColor];
+        leftLeg.sprite = legs[teamColor];
+        rightLeg.sprite = legs[teamColor];
+        head.sprite = heads[teamColor];
+
+    }
+
 }
