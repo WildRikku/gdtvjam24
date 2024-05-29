838798398ea1cc5743648ba7bd4cd72fade39b30
WildRikku <119439592+WildRikku@users.noreply.github.com>
2024-05-29 04:43:45 +0200

    follow eye only for active spider

--- Assets/Scripts/CrawlerBotLogic/SpiderEyeFollow.cs
+++ Assets/Scripts/CrawlerBotLogic/SpiderEyeFollow.cs
@@ -5,6 +5,7 @@ public class SpiderEyeFollow : MonoBehaviour
     private Camera mainCamera;
     public Transform eyeCenter; 
     public float radius = 1f;
+    public SpiderController spiderController;
 
     private void Awake()
     {
@@ -13,6 +14,8 @@ public class SpiderEyeFollow : MonoBehaviour
 
     void Update()
     {
+        if (!spiderController.isActive) return;
+        
         Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, eyeCenter.position.z - mainCamera.transform.position.z));
         Vector3 direction = mousePosition - eyeCenter.position;
         direction.z = 0; 
