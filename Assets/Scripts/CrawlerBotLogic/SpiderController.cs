767a65eaa6d3caf24e2f05dfad307d87c50bfd5a
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2024-05-30 01:13:23 +0200

    some explosion stuff and spider canvas + boost particle

--- Assets/Scripts/CrawlerBotLogic/SpiderController.cs
+++ Assets/Scripts/CrawlerBotLogic/SpiderController.cs
@@ -5,17 +5,27 @@ public class SpiderController : MonoBehaviour
     public float speed = 1f;
     private Rigidbody2D rb;
     public bool isActive;
+    public ParticleSystem[] boostParticleSystem;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        //TODO
+        rb.AddForce(Vector2.right * 50f);
+        Invoke(nameof(TODOSmallForce), 1f);
+    }
+
+    private void TODOSmallForce()
+    {
+        rb.AddForce(Vector2.up * 100f);
     }
 
     private void FixedUpdate()
     {
         if (!isActive) return;
-        
-        if(Mathf.Abs(rb.velocity.x) < speed)
+
+        if (Mathf.Abs(rb.velocity.x) < speed)
         {
             if (Input.GetKey(KeyCode.RightArrow))
             {
@@ -25,11 +35,12 @@ public class SpiderController : MonoBehaviour
             {
                 rb.AddForce(-Vector2.right * 50f);
             }
-            if (Input.GetKey(KeyCode.Space))
-            {
-                rb.AddForce(Vector2.up * 25f);
-            }
-
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * 25f);
+            boostParticleSystem[0].Emit(1);
+            boostParticleSystem[1].Emit(1);
         }
     }
 }
--- Assets/Scripts/CrawlerBotLogic/Weapon/ExplosionFX.cs
+++ Assets/Scripts/CrawlerBotLogic/Weapon/ExplosionFX.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class ExplosionFX : MonoBehaviour
 {
     private Animator expAnimator;
+    private SpriteRenderer spriteRenderer;
 
     void Start()
     {
         expAnimator = GetComponent<Animator>();
         expAnimator.SetTrigger("TrExplode");
+        spriteRenderer = GetComponent<SpriteRenderer>();
 
         Invoke(nameof(DestroyFX), 5f);
+    }
+
+    private void Update()
+    {
+        var animStateInfo = expAnimator.GetCurrentAnimatorStateInfo(0);
+        var NTime = animStateInfo.normalizedTime;
+        if (NTime > 0.95) spriteRenderer.enabled = false;
     }
 
     private void DestroyFX()
