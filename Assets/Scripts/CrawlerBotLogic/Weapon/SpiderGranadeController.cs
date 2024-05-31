f5db92440c3347fd36c03351af9a1c7060127f8e
Prayer90 <95182640+nightPrayer90@users.noreply.github.com>
2024-05-31 06:41:37 +0200

    add sounds for mainMenu and level sprites; add new granade thrower sprites and sprites for crosshairs

---
+++ Assets/Scripts/CrawlerBotLogic/Weapon/SpiderGranadeController.cs
@@ -0,0 +1,100 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpiderGranadeController : RotatingWeapon
+{
+    public Transform shotTriggerPoint;
+    public GameObject bombPrefab;
+
+    public float shootingSpeed = 10f;
+    private float shootingforce = 5f;
+
+    public ParticleSystem muzzleParticle;
+    public CanvasGroup speedBarCG;
+    public CanvasGroup crosshairCG;
+    public Image speedBar;
+
+    private bool isShooting;
+    private bool isShootingState;
+    private bool shootigReset = false; //TODO
+
+    private void Awake()
+    {
+        battleField = GameObject.Find("GameManagement").GetComponent<BattleField>();
+        rotationTempSpeed = rotationSpeed;
+    }
+
+    private void Start()
+    {
+        battleField = GameObject.Find("GameManagement").GetComponent<BattleField>();
+        speedBarCG.alpha = 0;
+        crosshairCG.alpha = 0;
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+        
+        if (isShootingState && isRotating) ShootingState();
+
+        if (Input.GetKeyDown(activationKey) && isShootingState == false && isShooting == false && shootigReset == false)
+        {
+            if (isRotating)
+            {
+                isShootingState = true;
+                speedBarCG.DOFade(1, 0.2f);
+            }
+            else
+            {
+                isRotating = true;
+                crosshairCG.alpha = 1;
+            }
+        }
+
+        if (isRotating && isShootingState == false) Rotate();
+    }
+
+    private void ShootingState()
+    {
+        float pendulumFrequency = 0.5f;
+
+        shootingforce = Mathf.Lerp(0, 1, (Mathf.Sin(Time.time * pendulumFrequency * Mathf.PI * 2) + 1) / 2);
+        speedBar.fillAmount = shootingforce;
+
+        if (Input.GetKeyDown(activationKey))
+        {
+            Invoke(nameof(TriggerShot), 0.1f);
+            isShooting = true;
+            isShootingState = false;
+        }
+    }
+    
+    private void TriggerShot()
+    {
+        ProjectileCount = 1;
+        muzzleParticle.Emit(40);
+        GameObject bomb = SpawnProjectile(bombPrefab, shotTriggerPoint.position, shotTriggerPoint.rotation);
+
+        Rigidbody2D rb = bomb.GetComponent<Rigidbody2D>();
+        shootingforce = 5f + (shootingSpeed * shootingforce * 1.8f);
+
+        if (rb != null)
+        {
+            rb.AddForce(shotTriggerPoint.up * shootingforce, ForceMode2D.Impulse);
+        }
+
+        speedBarCG.DOFade(0, 0.2f);
+        crosshairCG.DOFade(0, 0.2f);
+
+        isRotating = false;
+        isShooting = false;
+        shootigReset = true;
+        Invoke(nameof(InvokeShootingReset), 3f);
+    }
+
+    private void InvokeShootingReset()
+    {
+        shootigReset = false;
+    }
+}
