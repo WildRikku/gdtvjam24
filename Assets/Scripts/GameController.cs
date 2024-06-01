a739385cb069006bfef30c9de19ebce3c2cef8d7
WildRikku <119439592+WildRikku@users.noreply.github.com>
2024-06-01 05:07:48 +0200

    coding style

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,62 +2,54 @@ using System;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
-using UnityEngine.Serialization;
 using Random = UnityEngine.Random;
 
 public delegate void GameplayEvent(GameController gameController);
 
-public class GameController : MonoBehaviour
-{
+public class GameController : MonoBehaviour {
     public List<Team> teams;
     public CinemachineVirtualCamera cinemachineVirtualCamera;
     public GameMenuController menuController;
-    
+
     public event GameplayEvent MatchStarted;
     public event GameplayEvent TurnStarted;
 
     public int activeTeam;
     private bool _trackingProjectile;
 
-    public List<string> botNames;  // use in PlayerController
+    public List<string> botNames; // use in PlayerController
 
-    private void Awake()
-    {
+    private void Awake() {
         Invoke(nameof(MatchBegin), 2f);
         bool[] usedTeamColors = new bool[4];
-        
-        for (short i = 0; i < teams.Count; i++)
-        {
+
+        for (short i = 0; i < teams.Count; i++) {
             int teamColor;
-            do
-            {
+            do {
                 teamColor = Random.Range(0, 4);
             } while (usedTeamColors[teamColor]);
 
             teams[i].teamColor = teamColor;
-            if (menuController != null)
-            {
+            if (menuController != null) {
                 menuController.teamColorIndex[i] = teamColor;
             }
+
             usedTeamColors[teamColor] = true;
-            
+
             teams[i].index = i;
             teams[i].TeamUpdated += OnTeamUpdated;
         }
-        
+
         menuController.TurnTimeIsUp += OnTurnTimeIsUp;
     }
 
-    private void OnTurnTimeIsUp(object sender, EventArgs e)
-    {
+    private void OnTurnTimeIsUp(object sender, EventArgs e) {
         teams[activeTeam].EndTurn(true);
         EndTurn(true);
     }
 
-    private void OnTeamUpdated(short teamindex, int membercount)
-    {
-        if (membercount == 0)
-        {
+    private void OnTeamUpdated(short teamindex, int membercount) {
+        if (membercount == 0) {
             Debug.Log("Game over");
             menuController.ShowGameEndHUD(teamindex);
 /*#if UNITY_EDITOR
@@ -66,11 +58,9 @@ public class GameController : MonoBehaviour
         }
     }
 
-    private void MatchBegin()
-    {
+    private void MatchBegin() {
         activeTeam = 0;
-        foreach (Team t in teams)
-        {
+        foreach (Team t in teams) {
             t.TurnEnded += OnTurnEnded;
         }
 
@@ -78,14 +68,13 @@ public class GameController : MonoBehaviour
         Turn();
     }
 
-    private void Turn()
-    {
+    private void Turn() {
         TurnStarted?.Invoke(this);
 
         AudioManager.Instance.PlaySFX("CameraSwipe");
-        cinemachineVirtualCamera.Follow = teams[activeTeam].GetActivePlayer().transform; 
+        cinemachineVirtualCamera.Follow = teams[activeTeam].GetActivePlayer().transform;
         // Give control to the next character of the active player
-        
+
         // Wait for player action
         teams[activeTeam].PlayerAction();
 
@@ -94,8 +83,7 @@ public class GameController : MonoBehaviour
         if (pw != null) pw.ProjectileFired += OnProjectileFired;
     }
 
-    private void OnProjectileFired(GameObject projectile)
-    {
+    private void OnProjectileFired(GameObject projectile) {
         menuController.HideWeaponHUD();
 
         if (_trackingProjectile) return;
@@ -103,13 +91,11 @@ public class GameController : MonoBehaviour
         cinemachineVirtualCamera.Follow = projectile.transform;
     }
 
-    private void OnTurnEnded(object sender, EventArgs e)
-    {
+    private void OnTurnEnded(object sender, EventArgs e) {
         EndTurn();
     }
 
-    private void EndTurn(bool force = false)
-    {
+    private void EndTurn(bool force = false) {
         _trackingProjectile = false;
         activeTeam = (activeTeam == 1) ? 0 : 1;
         Debug.Log("wait for next turn, next up is team " + activeTeam);
--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -6,37 +6,34 @@ using Random = UnityEngine.Random;
 
 public delegate void TeamUpdated(short teamIndex, int memberCount);
 
-public class Team : MonoBehaviour
-{
+public class Team : MonoBehaviour {
     public const short MaxTeamMembers = 4;
-    
+
     public GameObject playerPrefab;
     private Dictionary<short, PlayerController> _members;
     private int _activeMember;
     
     public List<GameObject> weaponPrefabs;
     public List<Weapon> weapons;
-    public event EventHandler TurnEnded;
-    public event TeamUpdated TeamUpdated;
+    
     [HideInInspector]
     public int teamColor;
-
     public short index;
-
     public Collider2D spawnZone;
-
+    
     private GameController _gameController;
+    
+    public event EventHandler TurnEnded;
+    public event TeamUpdated TeamUpdated;
 
-
-    void Start()
-    {
+    void Start() {
         _members = new();
         _gameController = GameObject.Find("GameManagement").GetComponent<GameController>();
 
         Bounds bounds = spawnZone.bounds;
-        for (short i = 0; i < MaxTeamMembers; i++)
-        {
-            GameObject newPlayer = Instantiate(playerPrefab, bounds.min + bounds.size/MaxTeamMembers * i, Quaternion.identity, transform);
+        for (short i = 0; i < MaxTeamMembers; i++) {
+            GameObject newPlayer = Instantiate(playerPrefab, bounds.min + bounds.size / MaxTeamMembers * i,
+                Quaternion.identity, transform);
             PlayerController pc = newPlayer.GetComponent<PlayerController>();
             SpiderChangeTeamColor spiderChangeTeamColor = newPlayer.GetComponentInChildren<SpiderChangeTeamColor>();
             spiderChangeTeamColor.teamColor = teamColor;
@@ -44,58 +41,51 @@ public class Team : MonoBehaviour
             pc.mainWeapon = weaponPrefabs[Random.Range(0, weaponPrefabs.Count)];
             pc.index = i;
             pc.HealthUpdated += TeamMemberOnHealthUpdated;
-            if (_gameController.botNames.Count > 0)
-            {
+            if (_gameController.botNames.Count > 0) {
                 int value = Random.Range(0, _gameController.botNames.Count);
                 pc.botName = _gameController.botNames[value];
                 _gameController.botNames.RemoveAt(value);
             }
+
             _members.Add(i, pc);
         }
 
-        foreach (GameObject go in weaponPrefabs)
-        {
+        foreach (GameObject go in weaponPrefabs) {
             weapons.Add(go.GetComponent<Weapon>());
         }
 
         _activeMember = 0;
     }
 
-    private void TeamMemberOnHealthUpdated(PlayerController pc)
-    {
-        if (pc.Health <= 0)
-        {
+    private void TeamMemberOnHealthUpdated(PlayerController pc) {
+        if (pc.Health <= 0) {
             // RIP
             _members.Remove(pc.index);
             TeamUpdated?.Invoke(index, _members.Count);
         }
     }
 
-    public void PlayerAction()
-    {
+    public void PlayerAction() {
         _members.ElementAt(_activeMember).Value.Attack();
         _members.ElementAt(_activeMember).Value.TurnFinished += OnTurnFinished;
     }
 
-    public PlayerController GetActivePlayer()
-    {
+    public PlayerController GetActivePlayer() {
         if (_activeMember >= _members.Count) _activeMember = 0; // a team member died between turns
         return _members.ElementAt(_activeMember).Value;
     }
 
-    private void OnTurnFinished(PlayerController pc)
-    {
+    private void OnTurnFinished(PlayerController pc) {
         pc.TurnFinished -= OnTurnFinished;
         EndTurn();
         TurnEnded?.Invoke(this, EventArgs.Empty);
     }
 
-    public void EndTurn(bool force = false)
-    {
-        if (force)
-        {
+    public void EndTurn(bool force = false) {
+        if (force) {
             _members.ElementAt(_activeMember).Value.EndTurn();
         }
+
         _activeMember++;
         if (_activeMember == _members.Count)
             _activeMember = 0;
