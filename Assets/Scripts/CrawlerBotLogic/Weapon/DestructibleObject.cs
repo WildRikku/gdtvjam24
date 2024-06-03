632e306008c6704d546ca626d66ca4d7c364ee7e
WildRikku <119439592+WildRikku@users.noreply.github.com>
2024-06-03 15:06:55 +0200

    Revert "Revert "crates that can be destroyed and explosive barrels that can be destroyed and cause damage. for #18. currently causes stack overflows, maybe, or Unity is broken""
    This reverts commit 90e2eeb4f5efa0d95fa64e33ecd77cf684230dfc.

---
+++ Assets/Scripts/CrawlerBotLogic/Weapon/DestructibleObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace CrawlerBotLogic.Weapon {
+    public class DestructibleObject : MonoBehaviour {
+        [field: SerializeField]
+        public float health { get; set; }
+
+        public GameObject destructionFXPrefab;
+        
+        public void Die() {
+            Instantiate(destructionFXPrefab);
+            Destroy(gameObject);
+        }
+
+        public void TakeDamage(float amount) {
+            health -= amount;
+            if (health <= 0) {
+                health = 0;
+                Die();
+            }
+        }
+    }
+}
